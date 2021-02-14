87a7d3d469d281a660fa4f192641907335997ca5
Byron Aquilino <baqui001@odu.edu>
2021-02-14 10:29:42 -0500

    primative gradle build to control the docker compose and load the database

---
+++ solution/DB/Schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS USERS (
+    `ID` int(11) NOT NULL,
+    `MOVE_PLAN_ID` int(11) DEFAULT NULL,
+    `FEEDBACK_ID` int(11) DEFAULT NULL,
+    `FIRST_NAME` varchar(50) NOT NULL,
+    `LAST_NAME` varchar(50) NOT NULL,
+    `EMAIL` varchar(50) NOT NULL,
+    `PHONE_NUMBER` varchar(50) DEFAULT NULL,
+    `PASSWORD` varchar(50) NOT NULL,
+    PRIMARY KEY (`ID`)
+    );
+
