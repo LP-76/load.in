92bec903e00163771ea4391e2aee3aba35660b7d
Byron Aquilino <baqui001@odu.edu>
2021-02-14 16:44:20 -0500

    attempt at trying to get retrofit to read from other service layer

---
+++ solution/app/app/src/main/java/com/example/loadin_app/data/TestClass.java
@@ -0,0 +1,18 @@
+package com.example.loadin_app.data;
+
+//import odu.edu.loadin.common.BoxService;
+//import retrofit2.Retrofit;
+
+public class TestClass {
+
+    public void TestSpace(){
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl("http://localhost:9000/")
+//                .build();
+//
+//        BoxService service = retrofit.create(BoxService.class);
+
+
+    }
+
+}
