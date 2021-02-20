1f2e4d9ca88c506284c8b262d2a5de267a903798
Byron Aquilino <baqui001@odu.edu>
2021-02-20 15:34:57 -0500

    amazing spinning triangle example!

--- solution/app/app/src/main/java/com/example/loadin_app/ui/opengl/TestGLRenderer.java
+++ solution/app/app/src/main/java/com/example/loadin_app/ui/opengl/TestGLRenderer.java
@@ -3,6 +3,7 @@ package com.example.loadin_app.ui.opengl;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -12,7 +13,17 @@ public class TestGLRenderer implements GLSurfaceView.Renderer {
     private final float[] vPMatrix = new float[16];
     private final float[] projectionMatrix = new float[16];
     private final float[] viewMatrix = new float[16];
+    private float[] rotationMatrix = new float[16];
 
+    public volatile float angle;
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float p_angle) {
+        angle = p_angle;
+    }
 
 
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
@@ -33,10 +44,21 @@ public class TestGLRenderer implements GLSurfaceView.Renderer {
         // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
 
+        float[] scratch = new float[16];
+        // Create a rotation transformation for the triangle
+        long time = SystemClock.uptimeMillis() % 4000L;
+        //float angle = 0.090f * ((int) time);
+        Matrix.setRotateM(rotationMatrix, 0, angle, 0, 0, -1.0f);
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the vPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, vPMatrix, 0, rotationMatrix, 0);
+
 
 
         Triangle test = new Triangle();
-        test.draw(vPMatrix);
+        test.draw(scratch);
 
     }
 
