364461de513bd84031fc676603bed0bb5e3a846a
Crusaton <paulmichaelrod@gmail.com>
2021-03-04 16:08:48 -0500

    Redid how the layout is to allow for video and image views to overlap and have their visibility toggled based off what file is detected. Also changed the logic of the activity.

--- solution/app/app/src/main/java/com/example/loadin_app/TipsAndTricksActivity.java
+++ solution/app/app/src/main/java/com/example/loadin_app/TipsAndTricksActivity.java
@@ -1,6 +1,8 @@
 package com.example.loadin_app;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -13,6 +15,7 @@ import android.view.View;
 import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -49,12 +52,13 @@ import java.io.IOException;
 public class TipsAndTricksActivity extends AppCompatActivity {
 
     private TextView mTextView;
-    private static final String VIDEO_SAMPLE = "eric_andre";
+    private static final String mediaName = "cardboard.jpg";
     private VideoView mVideoView;
     private TextView mBufferingTextView;
     private int mCurrentPosition = 0;
     private static final String PLAYBACK_TIME = "play_time";
     private Button searchForArticle;
+    private ImageView imageView;
     private EditText articleKeyword;
     private String keyword;
     private String value;
@@ -89,18 +93,34 @@ public class TipsAndTricksActivity extends AppCompatActivity {
             }
         });
 
-        mVideoView = findViewById(R.id.articleVideo);
 
-        if(savedInstanceState != null)
+        /*
+        Checks the file extension of media file to see if it is an image or video.
+        At the moment this is a very janky approach as only .jpg or .mp4 will be accepted.
+        Depending on what media type is found it will set their respective view to visible
+        If a video is detected then it will also start the mediaplayer activity.
+         */
+        if (mediaName.toString().endsWith(".jpg"))
         {
-            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+            mVideoView = findViewById(R.id.articleVideo);
+            imageView = findViewById(R.id.articleImage);
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setImageResource(R.drawable.cardboard);
         }
-        MediaController controller = new MediaController(this);
-        controller.setMediaPlayer(mVideoView);
-        mVideoView.setMediaController(controller);
-        mBufferingTextView = findViewById(R.id.buffering_textview);
-
-
+        else if (mediaName.toString().endsWith(".mp4"))
+        {
+            mVideoView = findViewById(R.id.articleVideo);
+            mVideoView.setVisibility(View.VISIBLE);
+            if(savedInstanceState != null)
+            {
+                mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+            }
+            MediaController controller = new MediaController(this);
+            controller.setMediaPlayer(mVideoView);
+            mVideoView.setMediaController(controller);
+            mBufferingTextView = findViewById(R.id.buffering_textview);
+            initializePlayer();
+        }
     }
 
     // Menu icons are inflated just as they were with actionbar
@@ -168,7 +188,11 @@ public class TipsAndTricksActivity extends AppCompatActivity {
 
         }
     }
-
+    /**
+     * Searches for an article based on keyword provided.
+     *
+     * @param keyword
+     */
     private void searchForArticle(String keyword)
     {
         System.out.println("Searching for an article with keyword: " + keyword + "!");
@@ -181,13 +205,6 @@ public class TipsAndTricksActivity extends AppCompatActivity {
             TextView mArticleTitle = findViewById(R.id.articleTitle);
             mArticleContent.setText(expertArticle.getArticleContent());
             mArticleTitle.setText(expertArticle.getArticleTitle());
-           System.out.println("Article Keyword is: " + expertArticle.getKeyword());
-           System.out.println("Article Content is: " + expertArticle.getArticleContent());
-           System.out.println("Article Title is: " + expertArticle.getArticleTitle());
-           System.out.println("Article File is: " + expertArticle.getVisualFile());
-           System.out.println("Article Created at: " + expertArticle.getCreatedAt());
-           System.out.println("Article Updated at: " + expertArticle.getUpdatedAt());
-
         }
         catch(Exception ex){
             System.out.println(ex);
@@ -195,17 +212,34 @@ public class TipsAndTricksActivity extends AppCompatActivity {
             //TODO: make the user aware
         }
     }
-    private Uri getMedia(String mediaName) {
-        if(URLUtil.isValidUrl(mediaName)) {
+
+    /**
+     * Will check if the media file being provided is a url or is locally stored
+     * TODO This is now broken and won't work with locally stored files? Not sure why yet. - Paul
+     * @param mediaName
+     * @return valid uri of the media file
+     */
+    private Uri getMedia(String mediaName)
+    {
+        if(URLUtil.isValidUrl(mediaName))
+        {
             return Uri.parse(mediaName);
-        }else {
+        }
+        else
+            {
             return Uri.parse("android.resource://" + getPackageName() + "/raw/" + mediaName);
         }
 
     }
 
+    /**
+     * Will initialize the video player being used for tips and tricks
+     * Will call the getMedia(String mediaName) function to retrieve file name
+     * Todo: Should probably modify this to have it accept the media name and then pass it into the getMedia method - Paul
+     * This is the main driver of the video playing feature.
+     */
     private void initializePlayer() {
-        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        Uri videoUri = getMedia(mediaName);
         mBufferingTextView.setVisibility(VideoView.VISIBLE);
         mVideoView.setVideoURI(videoUri);
 
@@ -224,13 +258,15 @@ public class TipsAndTricksActivity extends AppCompatActivity {
                 mVideoView.start();
             }
         });
-        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+        {
             @Override
-            public void onCompletion(MediaPlayer mp) {
+            public void onCompletion(MediaPlayer mediaPlayer) {
                 Toast.makeText(TipsAndTricksActivity.this, "Playback completed", Toast.LENGTH_SHORT).show();
-                mVideoView.seekTo(1);
+                mVideoView.seekTo(0);
             }
         });
+        releasePlayer();
 
     }
     @Override
@@ -239,126 +275,18 @@ public class TipsAndTricksActivity extends AppCompatActivity {
 
         outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
     }
-    private void releasePlayer(){
+    private void releasePlayer()
+    {
         mVideoView.stopPlayback();
     }
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        initializePlayer();
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        releasePlayer();
-    }
-    @Override
-    protected void onPause() {
+    protected void onPause()
+    {
         super.onPause();
 
         if(Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
             mVideoView.pause();
         }
     }
-/*
-    private void startLucene() throws IOException, ParseException {
-        // Specify the analyzer for tokenizing text.
-        //    The same analyzer should be used for indexing and searching
 
-        // StandardAnalyzer removes stop words from the input, which is a
-        // great help when processing naturally occurring text. StandardAnalyzer
-        // also converts text to lowercase (an important point to note because case
-        // issues can lead to subtle bugs in application code when searching for data
-        // indexed with StandardAnalyzer).
-        StandardAnalyzer analyzer = new StandardAnalyzer();
-
-        // IndexWriter is the main user-facing class responsible for indexing
-        // data in Lucene. IndexWriter is used for analyzing documents, opening
-        // directories, and writing the data to directories.
-        Directory index = new RAMDirectory(); //TODO: NEED TO REPLACE THIS SINCE IT IS DEPRECATED SEE API https://lucene.apache.org/core/8_8_1/core/index.html
-
-        // Holds all the configuration that is used to create an IndexWriter.
-        // Once IndexWriter has been created with this object, changes to this object will not affect the IndexWriter instance.
-        // For that, use LiveIndexWriterConfig that is returned from IndexWriter.getConfig().
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-        //An IndexWriter creates and maintains an index.
-        IndexWriter w = new IndexWriter(index, config);
-        addDoc(w, "Clothes", "Roll Clothes to Keep Things Compact", "Hi");
-        addDoc(w, "Map out", "Map out the best way to get to your new home.", "Whether you’re moving to NYC, across the country, across state lines, or just to a neighboring town, you’re going to need an efficient travel route so you don’t waste your move-in day sitting in gridlock traffic or pulling over three different times to type an address into your GPS.");
-        addDoc(w, "electronics", "Check to see if you have original boxes for your electronics", "Check to see if you stashed these boxes somewhere — attic? Garage? If you don’t have them, make a list of what you’ll need to buy or borrow to properly cushion your stuff.\n Quilted blankets, bubble wrap, and sturdy tape all work well to protect TVs and similarly delicate items.");
-        addDoc(w, "heavy item", "Use small boxes for heavy items.", "It sounds obvious, but if you’ve ever known the struggle that is carrying a large cardboard box stuffed full of college textbooks across a parking lot, then you also know this advice cannot be overstated.");
-        w.close();
-
-        // the quertString is the keyword we are matching. If you look at our addDoc function, the param named keyword is what we are searching.
-        String queryString = "heavy";
-
-        // the "keyword" arg below specifies the default field to use when querying data. Currently I have three fields we could use: keyword, title, and article. Either of these can replace keyword
-        Query query = new QueryParser("keyword", analyzer).parse(queryString);
-
-        // limit of the amount of tips to display
-        int hitsPerPage = 10;
-
-        //used to read the underlying index using the Directory and corresponding abstractions.
-        IndexReader reader = DirectoryReader.open(index);
-
-        // IndexSearcher is the abstraction present in Lucene that executes
-        // search over a single Lucene index. IndexSearcher is opened on top
-        // of IndexReader, which is used to read the underlying index using
-        // the Directory and corresponding abstractions.
-        IndexSearcher searcher = new IndexSearcher(reader);
-
-        //TopDocs is the representation of the top documents that match the given
-        //query. They are a generic representation and do not necessarily depend
-        //on the underlying algorithm used to calculate how the top documents
-        //are calculated. TopDocs consist of two components: scoreDocs (the
-        //documentIDs of the top N hits, where N was the requested value) and the
-        //score of each of those hits.
-        TopDocs docs = searcher.search(query, hitsPerPage);
-
-
-        ScoreDoc[] hits = docs.scoreDocs;
-
-        // printing off the result to the console.
-        Log.d("test","Found " + hits.length + " hits.");
-        for(int i=0; i < hits.length; ++i) {
-
-            int docId = hits[i].doc;
-            Document doc = searcher.doc(docId);
-            Log.d("test",(i + 1) + ". Keyword: " + doc.get("keyword") + "\tExpect Tip Title: " + doc.get("title") +
-                    "\tExpect Tip Article: " + doc.get("article"));
-        }
-
-        reader.close();
-    }
-    */
-
-
-    /**
-     * Used to add items to the Document element.
-     *
-     * @param w - creates and maintains an index.
-     * @param keyword - keyword we are searching for inside
-     * @param title - title of the articles
-     * @param article - the meat of the article
-     * @throws IOException
-     */
-    /*
-    private static void addDoc(IndexWriter w, String keyword, String title, String article) throws IOException {
-        Document doc = new Document();
-
-        // using TextField that is indexed and tokenized, without term vectors.
-        doc.add(new TextField("keyword", keyword, Field.Store.YES));
-
-        // using a string field for title because we don't want it tokenized
-        doc.add(new StringField("article", article, Field.Store.YES));
-
-        // using a string field for title because we don't want it tokenized
-        doc.add(new StringField("title", title, Field.Store.YES));
-        w.addDocument(doc);
-    }
-
-    */
 }
