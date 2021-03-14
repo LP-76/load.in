a554ee7d94d1e94086de482cb4d0d4fb01e4218b
Byron Aquilino <baqui001@odu.edu>
2021-03-14 17:16:19 -0400

    fix for load plan box

--- solution/common/src/main/java/odu/edu/loadin/common/LoadPlanBox.java
+++ solution/common/src/main/java/odu/edu/loadin/common/LoadPlanBox.java
@@ -1,10 +1,22 @@
 package odu.edu.loadin.common;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
 
 @XmlRootElement(name="Data")
-public class LoadPlanBox extends Inventory
+public class LoadPlanBox
 {
+    protected int id;
+    protected int userID;
+    protected int boxID;
+    protected String description;
+    protected float width;
+    protected float height;
+    protected float length;
+    protected int fragility;
+    protected double weight;
+    protected Date createdAt;
+    protected Date updatedAt;
     private float xOffset,yOffset,zOffset;
     private int  loadNumber,stepNumber;
 
@@ -71,5 +83,93 @@ public class LoadPlanBox extends Inventory
 
     public void setStepNumber(int stepNumber) {
         this.stepNumber = stepNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getBoxID() {
+        return boxID;
+    }
+
+    public void setBoxID(int boxID) {
+        this.boxID = boxID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public int getFragility() {
+        return fragility;
+    }
+
+    public void setFragility(int fragility) {
+        this.fragility = fragility;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
