8c6a551e55880ff6f2360cdf3e47a3f91d145a1d
Chris <cmill029@odu.edu>
2021-04-13 01:03:38 -0400

    First pass at preloaded John Smith inventory

--- solution/DB/TableData.sql
+++ solution/DB/TableData.sql
@@ -14,8 +14,87 @@ INSERT INTO ROLE (DESCRIPTION)
 VALUES ('Standard User'), ('Administrator User');
 
 INSERT INTO USER_INVENTORY_ITEM (USER_ID, BOX_ID, BOX_WIDTH, BOX_LENGTH, BOX_HEIGHT, ITEM_DESCRIPTION, FRAGILITY, WEIGHT, CREATED_AT, UPDATED_AT, STATUS, ROOM, ITEM_LIST)
-VALUES (1, 1, 3, 3, 3, 'Mothers Clothes', '2', 234.5, NOW(), NOW(), 'At Source', 'Bedroom', 'thong, leggings, sweatshirt'),
-       (1, 1, 48, 48, 60, 'Fridge', '2', 10, NOW(), NOW(), 'At Source', 'kitchen', 'various food items');
+VALUES (2, 1, 18, 18, 16, 'Moms Winter Clothes', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'leggings, sweatshirts'),
+              (2, 2, 18, 18, 16, 'Moms Summer Clothes', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'tshirts, shorts'),
+              (2, 3, 18, 18, 16, 'Moms Other Clothes', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'other clothes'),
+              (2, 4, 18, 18, 16, 'Moms Everyday Clothes', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'jeans, socks'),
+              (2, 5, 24, 24, 24, 'Parents Bedding', '1', 1, NOW(), NOW(), 'At Source', 'parents bedroom', 'sheets, pillow cases, comforter'),
+              (2, 6, 24, 24, 24, 'Parents Pillows', '1', 1, NOW(), NOW(),'At Source', 'parents bedroom', 'pillows'),
+              (2, 7, 60, 20, 34, 'Parents Dresser', '1', 5, NOW(), NOW(), 'At Source', 'parents bedroom', 'clothing dresser'),
+              (2, 8, 70, 80, 11, 'Parents Mattress', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'king mattress'),
+              (2, 9, 18, 18, 24, 'Parents Desk Lamp', '5', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'lamp base, lamp shade'),
+              (2, 10, 45, 4, 24, 'Bedroom TV', '5', 3, NOW(), NOW(), 'At Source', 'parents bedroom', 'flatscreen TV'),
+              (2, 11, 18, 18, 16, 'Dads Winter Clothes', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'khaki pants, sweatshirts'),
+              (2, 12, 18, 18, 16, 'Dads Summer Clothes', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'tshirts, basketball shorts'),
+              (2, 13, 18, 18, 16, 'Dads Other Clothes', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'other clothes'),
+              (2, 14, 18, 18, 16, 'Dads Everyday Clothes', '1', 2, NOW(), NOW(), 'At Source', 'parents bedroom', 'jeans, socks'),
+
+              (2, 15, 18, 18, 16, 'Sallys Winter Clothes', '1', 2, NOW(), NOW(), 'At Source', 'Sallys bedroom', 'leggings, sweatshirts'),
+              (2, 16, 18, 18, 16, 'Sallys Summer Clothes', '1', 2, NOW(), NOW(), 'At Source', 'Sallys bedroom', 'tshirts, basketball shorts, short shorts'),
+              (2, 17, 18, 18, 16, 'Sallys Other Clothes', '1', 2, NOW(), NOW(), 'At Source', 'Sallys bedroom', 'other clothes'),
+              (2, 18, 18, 18, 16, 'Sallys Everyday Clothes', '1', 2, NOW(), NOW(), 'At Source', 'Sallys bedroom', 'jeans, socks'),
+              (2, 19, 24, 24, 24, 'Sallys Bedding', '1', 1, NOW(), NOW(), 'At Source', 'Sallys bedroom', 'sheets, pillow cases, comforter'),
+              (2, 20, 24, 24, 24, 'Sallys Pillows', '1', 1, NOW(), NOW(),'At Source', 'Sallys bedroom', 'pillows'),
+              (2, 21, 60, 20, 34, 'Sallys Dresser', '1', 5, NOW(), NOW(), 'At Source', 'Sallys bedroom', 'clothing dresser'),
+              (2, 22, 50, 75, 11, 'Sallys Mattress', '1', 2, NOW(), NOW(), 'At Source', 'Sallys bedroom', 'king mattress'),
+              (2, 23, 18, 18, 24, 'Sallys Desk Lamp', '5', 2, NOW(), NOW(), 'At Source', 'Sallys bedroom', 'lamp base, lamp shade'),
+
+              (2, 24, 36, 30, 70, 'Refrigerator', '1', 5, NOW(), NOW(), 'At Source', 'kitchen', 'empty fridge'),
+              (2, 25, 18, 18, 24, 'Kitchen Cleaning Supplies', '3', 3, NOW(), NOW(), 'At Source', 'kitchen', 'soap, sponges, dustpan'),
+              (2, 26, 18, 18, 24, 'Kitchen Towels', '1', 2, NOW(), NOW(), 'At Source', 'kitchen', 'paper towels, napkins, kitchen towels'),
+              (2, 27, 24, 18, 24, 'Kitchen Pans', '3', 4, NOW(), NOW(), 'At Source', 'kitchen', '8 pans, 5 lids'),
+              (2, 28, 24, 18, 24, 'Kitchen Pots', '3', 4, NOW(), NOW(), 'At Source', 'kitchen', '3 pots, 2 lids'),
+              (2, 29, 16, 12, 12, 'Cooking Utensils', '2', 3, NOW(), NOW(), 'At Source', 'kitchen', '8 spatulas, 2 whisks, 5 wood spoons'),
+              (2, 30, 16, 12, 12, 'Cooking Utensils', '2', 3, NOW(), NOW(), 'At Source', 'kitchen', 'rubber spatulas, ice cream scooper, ladle'),
+              (2, 31, 18, 18, 24, 'Fine China', '5', 3, NOW(), NOW(), 'At Source', 'kitchen', 'plates, cups'),
+              (2, 32, 18, 18, 24, 'Dishes', '4', 3, NOW(), NOW(), 'At Source', 'kitchen', 'plates, glasses'),
+              (2, 33, 18, 18, 24, 'Dishes', '4', 3, NOW(), NOW(), 'At Source', 'kitchen', 'bowls, plates'),
+              (2, 34, 24, 18, 24, 'Casserole Dishes', '3', 4, NOW(), NOW(), 'At Source', 'kitchen', '3 casserole dishes, 2 cookie sheets'),
+              (2, 35, 24, 18, 24, 'Casserole Dishes', '3', 4, NOW(), NOW(), 'At Source', 'kitchen', '3 casserole dishes, 2 cookie sheets'),
+              (2, 36, 24, 18, 24, 'Stand Mixer', '2', 4, NOW(), NOW(), 'At Source', 'kitchen', 'stand mixer'),
+              (2, 37, 24, 18, 24, 'Coffee Machine', '4', 2, NOW(), NOW(), 'At Source', 'kitchen', 'coffee machine', 'coffee filters, coffee'),
+              (2, 38, 24, 18, 24, 'Silverware', '1', 3, NOW(), NOW(), 'At Source', 'kitchen', 'forks, knives, spoons'),
+
+              (2, 39, 24, 24, 24, 'Guest Room Bedding', '1', 1, NOW(), NOW(), 'At Source', 'guest bedroom', 'sheets, pillow cases, comforter'),
+              (2, 40, 24, 24, 24, 'Guest Room Pillows', '1', 1, NOW(), NOW(),'At Source', 'guest bedroom', 'pillows'),
+              (2, 41, 60, 20, 34, 'Guest Room Dresser', '1', 5, NOW(), NOW(), 'At Source', 'guest bedroom', 'clothing dresser'),
+              (2, 42, 70, 80, 11, 'Guest Room Mattress', '1', 2, NOW(), NOW(), 'At Source', 'guest bedroom', 'king mattress'),
+              (2, 43, 18, 18, 24, 'Guest Room Desk Lamp', '5', 2, NOW(), NOW(), 'At Source', 'guest bedroom', 'lamp base, lamp shade'),
+              (2, 44, 45, 4, 24, 'Guest Room TV', '5', 3, NOW(), NOW(), 'At Source', 'guest bedroom', 'flatscreen TV'),
+
+              (2, 45, 45, 4, 24, 'Living Room TV', '5', 3, NOW(), NOW(), 'At Source', 'living room', 'flatscreen TV'),
+              (2, 40, 24, 24, 24, 'Living Room Pillows', '1', 1, NOW(), NOW(),'At Source', 'living room', 'pillows'),
+              (2, 41, 18, 18, 24, 'Blankets', '1', 2, NOW(), NOW(), 'At Source', 'living room', 'blankets'),
+              (2, 42, 18, 18, 24, 'Board games', '1', 2, NOW(), NOW(), 'At Source', 'monopoly, clue, scrabble'),
+              (2, 43, 16, 12, 12, 'Books', '2', 3, NOW(), NOW(), 'At Source', 'living room', 'Harry Potter, Fahrenheit 451'),
+              (2, 44, 16, 12, 12, 'More Books', '2', 3, NOW(), NOW(), 'At Source', 'living room', 'Tom Jones, Pride and Prejudice, Mrs Dalloway'),
+              (2, 45, 16, 12, 12, 'More Books', '2', 3, NOW(), NOW(), 'At Source', 'living room', 'Great Expectations, Jane Eyre, Bleak House'),
+              (2, 46, 16, 12, 12, 'More Books', '2', 3, NOW(), NOW(), 'At Source', 'living room', 'Wuthering Heights, Oryx and Crake, Atonement'),
+              (2, 47, 16, 12, 12, 'More Books', '2', 3, NOW(), NOW(), 'At Source', 'living room', 'MaddAddam, The Year of the Flood, Nineteen Eighty-four'),
+              (2, 48, 36, 36, 4, 'Dog bed', '1', 2, NOW(), NOW(), 'At Source', 'living room', 'dog bed'),
+              (2, 49, 16, 12, 12, 'Dog bowls', '1', 2, NOW(), NOW(), 'At Source', 'living room', 'water bowl, food bowl'),
+              (2, 50, 24, 18, 24, 'Dog food', '1', 4, NOW(), NOW(), 'At Source', 'living room', 'dog food'),
+              (2, 51, 18, 18, 24, 'Dog toys', '1', 2, NOW(), NOW(), 'At Source', 'living room', 'ducky, leash, Gumby'),
+
+              (2, 52, 16, 12, 12, 'Toiletries', '2', 2, NOW(), NOW(), 'At Source', 'parents bathroom', 'toilet paper, soap, shampoo, toothpaste'),
+              (2, 53, 16, 12, 12, 'Toiletries', '2', 2, NOW(), NOW(), 'At Source', 'Sallys bathroom', 'toilet paper, soap, shampoo, toothpaste'),
+              (2, 54, 16, 12, 12, 'Toiletries', '2', 2, NOW(), NOW(), 'At Source', 'bathroom', 'toilet paper, soap, bandaids, advil'),
+              (2, 55, 16, 12, 12, 'Cleaning Supplies', '4', 2, NOW(), NOW(), 'At Source', 'utility room', 'bleach, windex, trash bags'),
+              (2, 56, 16, 12, 12, 'Laundry Supplies', '3', 2, NOW(), NOW(), 'At Source', 'utility room', 'detergent, drier sheets'),
+
+              (2, 57, 16, 12, 12, 'Parents electronics', '4', 2, NOW(), NOW(), 'At Source', 'miscellaneous', 'laptop, chargers, keyboard'),
+              (2, 58, 16, 12, 12, 'Sallys electronics', '4', 2, NOW(), NOW(), 'At Source', 'miscellaneous', 'laptop, chargers, speakers'),
+              (2, 59, 24, 18, 24, 'Printer', '4', 2, NOW(), NOW(), 'At Source', 'miscellaneous', 'printer, ink'),
+
+              (2, 60, 16, 12, 12, 'Tools', '1', 3, NOW(), NOW(), 'At Source', 'garage', 'ratchets, nails, bolts, nuts'),
+              (2, 61, 16, 12, 12, 'Tools', '1', 3, NOW(), NOW(), 'At Source', 'garage', 'wrenches, screws, drill bits, hammer'),
+              (2, 62, 16, 12, 12, 'Tools', '1', 3, NOW(), NOW(), 'At Source', 'garage', 'drill, spade, shears'),
+
+              (2, 63, 18, 18, 24, 'Holiday Decorations', '2', 3, NOW(), NOW(), 'At Source', 'miscellaneous', 'Christmas decorations'),
+              (2, 64, 18, 18, 24, 'Holiday Decorations', '2', 3, NOW(), NOW(), 'At Source', 'miscellaneous', 'Halloween decorations'),
+              (2, 65, 18, 18, 24, 'Odds and ends', '1', 2, NOW(), NOW(), 'At Source', 'miscellaneous', 'towels, slippers, nerf ball');
+
+
 
 INSERT INTO EXPERT_TIP (KEYWORD, TITLE, CONTENT, IMAGE, VIDEO, COMMENTS, CREATED_AT, UPDATED_AT)
 VALUES
