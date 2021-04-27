72c0a538658d0ad1915aa14c2213f2a5e4cc120a
Crusaton <paulmichaelrod@gmail.com>
2021-04-26 20:21:55 -0400

    Added janet's second easter egg.

--- solution/DB/TableData.sql
+++ solution/DB/TableData.sql
@@ -132,7 +132,8 @@ VALUES
      ,('Pack a Box', 'How to pack a box', 'Ensure that all heavy items are at the bottom of the box. Make sure that you do not overload the box.', NULL, 'https://loadinvideohost.s3.amazonaws.com/prep_box.mp4', 'THIS TIP SUCKS', NOW(), NOW())
      ,('Fine China', 'How to pack fine china' , 'Ensure that the fine china is in a box with proper amount of packing material. Make sure that you do not overload the box.', NULL, 'https://loadinvideohost.s3.amazonaws.com/fine_china.mp4', 'THIS TIP SUCKS', NOW(), NOW())
      ,('Heavy', 'How to lift a heavy item', 'Ensure that you lift with your back. Make sure that you do not overload the box.', NULL, 'https://loadinvideohost.s3.amazonaws.com/heavy_real.mp4', 'THIS TIP SUCKS', NOW(), NOW())
-     ,('Christmas Ornaments', 'How to pack your christmas ornaments', 'Ensure that you properly package the ornaments. Use spare newspaper and pack the ornaments so that they are at the top of the box.', NULL, 'https://loadinvideohost.s3.amazonaws.com/packornament.mp4', 'LOADINNUMBA1', NOW(), NOW());
+     ,('Christmas Ornaments', 'How to pack your christmas ornaments', 'Ensure that you properly package the ornaments. Use spare newspaper and pack the ornaments so that they are at the top of the box.', NULL, 'https://loadinvideohost.s3.amazonaws.com/packornament.mp4', 'LOADINNUMBA1', NOW(), NOW())
+     ,('Grinchs heart', 'Peace through all of 411', 'Twas the night before the demo when all through the class, not a student was working, not even Byron.', NULL, 'https://loadinvideohost.s3.amazonaws.com/janetshearteditted.mp4', 'LOADINNUMBA1', NOW(), NOW());
 
 
 
